7dc91600245819afa5b2c9b7c2ce6c7705b23c31
LoveH <56201413+LoveHka@users.noreply.github.com>
2025-11-21 20:20:17 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ BLACK = (0, 0, 0)  # Цвета
 WHITE = (255, 255, 255)
 
 dino = Actor("dino1.png", pos=(WIDTH // 2, HEIGHT // 2))  # Создали динозавра
-animation_timer = 0
+
 
 def draw():
     screen.fill(WHITE)  # Заполняем фон экрана белым цветом
@@ -16,14 +16,7 @@ def draw():
 
 
 def update():
-    global animation_timer
-    animation_timer += 1
-    if animation_timer > 10:
-        if dino.image == "dino1.png":
-            dino.image = "dino2.png"
-        else:
-            dino.image = "dino1.png"
-        animation_timer = 0
+    
     if keyboard.w:
         dino.y -= 3
     if keyboard.s:
