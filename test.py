8e3d3e471e3eee7b7a4f0ae51744384ca8db7bf6
LoveH <56201413+LoveHka@users.noreply.github.com>
2025-11-21 20:17:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,29 @@
 import pgzrun
 import random
+
 WIDTH = 800
 HEIGHT = 600
 
-BLACK = (0, 0, 0) # Цвета
+BLACK = (0, 0, 0)  # Цвета
 WHITE = (255, 255, 255)
 
-dino = Actor("dino.png", pos = (WIDTH // 2, HEIGHT // 2)) # Создали динозавра
+dino = Actor("dino1.png", pos=(WIDTH // 2, HEIGHT // 2))  # Создали динозавра
+animation_timer = 0
 
 def draw():
-    screen.fill(WHITE) # Заполняем фон экрана белым цветом
+    screen.fill(WHITE)  # Заполняем фон экрана белым цветом
     dino.draw()
-    
+
+
 def update():
+    global animation_timer
+    animation_timer += 1
+    if animation_timer > 10:
+        if dino.image == "dino1.png":
+            dino.image = "dino2.png"
+        else:
+            dino.image = "dino1.png"
+        animation_timer = 0
     if keyboard.w:
         dino.y -= 3
     if keyboard.s:
@@ -21,4 +32,6 @@ def update():
         dino.x -= 3
     if keyboard.d:
         dino.x += 3
+
+
 pgzrun.go()
