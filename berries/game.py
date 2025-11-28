ab017ac44cfb2679cacd6d718bf710c4791a96f5
LoveH <56201413+LoveHka@users.noreply.github.com>
2025-11-28 20:21:17 +0300

    Update game.py

--- berries/game.py
+++ berries/game.py
@@ -1,6 +1,7 @@
 import pgzrun
 import time
 
+
 WIDTH = 800
 HEIGHT = 600
 BLACK = (0,0,0)
@@ -17,26 +18,28 @@ berrie_speed = 10
 # Функция принимает от нас позицию мышки
 def attack(mouse_pos): # Это наша собственная функция для атаки.
     global last_attack # Узнаём, КОГДА была последняя атака
-    if last_attack - time.time() > cooldown: # Если прошло достаточно времени
+    if time.time() - last_attack > cooldown: # Если прошло достаточно времени
         last_attack = time.time() # Обновляем таймер
         dx = mouse_pos[0] - hero.x # Разница по оси икс
         dy = mouse_pos[1] - hero.y # По оси игрек
-        distance = (dx^2 + dy^2)^0.5 # Расстояние между мышкой и героем
+        distance = (dx**2 + dy**2)**0.5 # Расстояние между мышкой и героем
         speed_x = berrie_speed * (dx / distance) # Скорость по оси икс
         speed_y = berrie_speed * (dy / distance) # Скорость по оси игрек
         berries.append(
             [ Rect(hero.x, hero.y, 10, 10), speed_x, speed_y ]
         )
-        
+
 def on_mouse_down(pos): # При нажатии мышки
     attack(pos) # Пробуем запустить нашу ягодку
-
 
 def draw():
     screen.fill(WHITE)
     screen.draw.filled_rect(hero, BLACK) # Рисуем персонажа
+    # Рисуем ягодки
+    for berr in berries:
+        screen.draw.filled_rect(berr[0], RED)
 def update():
-
+    pass
 
 
 
