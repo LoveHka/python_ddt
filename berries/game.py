98d494ece850ed39858737bf0578756b55549ac0
LoveH <56201413+LoveHka@users.noreply.github.com>
2025-11-28 20:11:45 +0300

    Update game.py

--- berries/game.py
+++ berries/game.py
@@ -7,7 +7,7 @@ BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255, 0, 0)
 
-hero = Rect(100, 100, 20, 40)
+hero = Rect(100, 300, 20, 40)
 
 cooldown = 0.5 # Это время перезарядки
 last_attack = 0 # Когда была последняя атака?
@@ -17,21 +17,24 @@ berrie_speed = 10
 # Функция принимает от нас позицию мышки
 def attack(mouse_pos): # Это наша собственная функция для атаки.
     global last_attack # Узнаём, КОГДА была последняя атака
-        if last_attack - time.time() > cooldown: # Если прошло достаточно времени
-            last_attack = time.time() # Обновляем таймер
-            dx = mouse_pos[0] - hero.x
-            dy = mouse_pos[1] - hero.y
-            distance = (dx^2 + dy^2)^0.5
-            speed_x = berrie_speed * (dx / distance)
-            speed_y = berrie_speed * (dy / distance)
-            berries.append(
-                [ Rect(hero.x, hero.y, 10, 10), speed_x, speed_y ]
-            )
+    if last_attack - time.time() > cooldown: # Если прошло достаточно времени
+        last_attack = time.time() # Обновляем таймер
+        dx = mouse_pos[0] - hero.x # Разница по оси икс
+        dy = mouse_pos[1] - hero.y # По оси игрек
+        distance = (dx^2 + dy^2)^0.5 # Расстояние между мышкой и героем
+        speed_x = berrie_speed * (dx / distance) # Скорость по оси икс
+        speed_y = berrie_speed * (dy / distance) # Скорость по оси игрек
+        berries.append(
+            [ Rect(hero.x, hero.y, 10, 10), speed_x, speed_y ]
+        )
+        
+def on_mouse_down(pos): # При нажатии мышки
+    attack(pos) # Пробуем запустить нашу ягодку
 
 
 def draw():
     screen.fill(WHITE)
-    screen.draw.filled_rect(hero, BLACK)
+    screen.draw.filled_rect(hero, BLACK) # Рисуем персонажа
 def update():
 
 
