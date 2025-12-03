378778b6469f9cd2ffe93e7b7877983ccced8c48
LoveH <56201413+LoveHka@users.noreply.github.com>
2025-12-03 16:09:13 +0300

    Update game.py

--- cowboy/game.py
+++ cowboy/game.py
@@ -1,3 +1,4 @@
+
 import pgzrun   # Импорт бибилиотеки с играми
 import random   # Импорт библиотеки случайных чисел
 
@@ -19,49 +20,63 @@ enemies = [] # Пустой список для врагов
 e_speed = 2 # Скорость врагов
 chanse = 10
 
+game_over = False
+
 def on_mouse_down(pos):     # Если щёлкнули мышкой
     # Математика для расчёта скорости пули
     dx = pos[0] - hero.x                    # Разница координат х мышки и героя
     dy = pos[1] - hero.y                     # Разница координат y мышки и героя
     distance = (dx**2 + dy**2)**0.5             # Расстояние между мышкой и героем
-    speed_x = b_speed * ( dx / distance )       # умножкаем модуль скорости на синус
-    speed_y = b_speed * ( dy / distance )       # умножаем модуль скорости на косинус
+    speed_x = b_speed * ( dx / (distance + 0.00001) )       # умножкаем модуль скорости на синус
+    speed_y = b_speed * ( dy / (distance + 0.00001) )       # умножаем модуль скорости на косинус
 
     bullets.append(
         [ Rect(hero.x, hero.y, 5, 5), speed_x, speed_y ] # Добавляем пулю
     )
 
-def draw():     # Функция для рисования 
-    screen.fill(GREEN)
-    screen.draw.filled_rect(hero, BLACK)
-    for b in bullets:
-        screen.draw.filled_rect(b[0], RED) # Рисуем каждую пульку красным цветом
-    for e in enemies:
-        screen.draw.filled_rect(e, RED) # Рисуем каждого врага красным цветом
+def draw():     # Функция для рисования
+    if not game_over:
+        screen.fill(GREEN) # Заполняем экран
+        screen.draw.filled_rect(hero, BLACK) # Рисуем персонажа
+        for b in bullets:
+            screen.draw.filled_rect(b[0], RED) # Рисуем каждую пульку красным цветом
+        for e in enemies:
+            screen.draw.filled_rect(e, RED) # Рисуем каждого врага красным цветом
+    else:
+        screen.fill(BLACK) # Заполняем экран
+        screen.draw.text("GAME OVER", center=(WIDTH // 2, HEIGHT // 2), color=RED, fontsize=100)
 
 def update():   # Функция для обновления данных
+    global game_over
+    if game_over:
+        return
 
-    if random.randint(0, 600) < chanse:
+    if random.randint(0, 600) < chanse: # Спавн врагов
         enemies.append(
             Rect(random.choice([-100, WIDTH + 100]), random.choice([-100, HEIGHT + 100]), 20, 20)
         )
 
-    for e in enemies:
+    for e in enemies: # Перемещение врагов
         dx =  hero.x - e.x   # Разница координат х мышки и героя
         dy =  hero.y - e.y   # Разница координат y мышки и героя
         distance = (dx ** 2 + dy ** 2) ** 0.5  # Расстояние между мышкой и героем
-        speed_x = e_speed * (dx / distance)  # умножкаем модуль скорости на синус
-        speed_y = e_speed * (dy / distance)  # умножаем модуль скорости на косинус
+
+        speed_x = e_speed * (dx / (distance + 0.00001))  # умножкаем модуль скорости на синус
+        speed_y = e_speed * (dy / (distance + 0.00001))  # умножаем модуль скорости на косинус
         e.x += speed_x
         e.y += speed_y
-        for b in bullets:  # Проверяем столкновения
+        for b in bullets:  # Проверяем столкновения врагов и пулек
             if e.colliderect(b[0]):
                 bullets.remove(b)
                 enemies.remove(e)
 
-    for b in bullets:
+        if e.colliderect(hero): # Столкновение с героем и конец игры
+            game_over = True
+
+
+
+    for b in bullets:  # Перемещает пули
         b[0].x += b[1]
         b[0].y += b[2]
 
 pgzrun.go()
-
