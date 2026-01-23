cb452d4f6bf7a69c841603ed401c5adab1db3950
LoveH <56201413+LoveHka@users.noreply.github.com>
2026-01-23 20:48:20 +0300

    Update main.py

--- reaction/main.py
+++ reaction/main.py
@@ -10,8 +10,10 @@ button_time = 0 # Сюда мы запишем, во сколько мы наж�
 reaction_time = 0   # Время реакции
 
 def start_game():
-    flag.configure(text="...ЛЕСОПИЛКА...")
+    global start_time
+    flag.configure(text="Жди...")
     window.after(randint(5000,10000), green_flag)
+    start_time = 0
     button.configure(bg = "red", command=end_game)
 
 def green_flag():
@@ -21,9 +23,13 @@ def green_flag():
 
 def end_game():
     global start_time, button_time, reaction_time
+    if start_time == 0:
+        flag.configure(text="Фальстарт!")
+        button.configure(command=start_game)
+        return
     button_time = time() # Отмечаем, когда кнопка была нажата
-    reaction_time = button_time - start_time # Узнаём время реакции
-    result = str(reaction_time)[:5] 
+    reaction_time = button_time - start_time  # Узнаём время реакции
+    result = str(reaction_time)[:5]
     flag.configure(text= f"Время реакции: {result} секунд" )
     button.configure(command=start_game)
 
@@ -34,7 +40,7 @@ flag = Label(
              )
 flag.pack(pady=20)
 
-button = Button(window, text="", width=10, height=2, bg="red", command=start_game)
+button = Button(window, text="", width=15, height=3, bg="red", command=start_game)
 button.pack(pady=10)
 
 
