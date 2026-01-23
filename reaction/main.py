2329de6351b3e0036cc8cae9ef34013940d0253b
LoveH <56201413+LoveHka@users.noreply.github.com>
2026-01-23 20:02:32 +0300

    Update main.py

--- reaction/main.py
+++ reaction/main.py
@@ -1,15 +1,27 @@
 from tkinter import *
 from random import randint
-from time import time
+from time import time, sleep
 
 window = Tk()               # Создаём окно
-window.geometry("300x300+1000+200")  # Задаём размеры
+window.geometry("400x300+1000+200")  # Задаём размеры
 
+start_time = 0  # Сюда мы потом запишем время начала теста на реакцию
+button_time = 0 # Сюда мы запишем, во сколько мы нажали на кнопку после начала
 
-flag = Label(window, text="Нажмите на кнопку для старта...")
+def start_game():
+    global start_time, button_time
+    flag.configure(text="...ЛЕСОПИЛКА...")
+    sleep(randint(5000, 25000)/1000)
+    button.configure(bg="green")
+
+flag = Label(
+    window,
+    text="Нажмите на кнопку для старта...",
+    font=("Arial", 16)
+             )
 flag.pack(pady=20)
 
-button = Button(window, text="", width=10, height=2, bg="red")
+button = Button(window, text="", width=10, height=2, bg="red", command=start_game)
 button.pack(pady=10)
 
 
