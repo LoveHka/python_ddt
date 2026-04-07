6dba06a4f9c11d7b6b5042e8879d59089afe390e
LoveH <56201413+LoveHka@users.noreply.github.com>
2026-04-07 18:11:30 +0300

    Update client.py

--- hackers/client.py
+++ hackers/client.py
@@ -1,13 +1,55 @@
 import socket
+import threading
+import tkinter as tk
 
-HOST = "127.0.0.1"
-PORT = 5000
+from pyexpat.errors import messages
 
-client = socket.socket()
-client.connect((HOST, PORT))
+HOST = "192.168.31.232"
+PORT = 7000
 
-print("Подключено к серверу. Пиши сообщения:")
+# ---- SOCKET ------
+sock = socket.socket()
+sock.connect((HOST, PORT))
 
-while True:
-    msg = input("> ")
-    client.send(msg.encode())
+# --- ИНтерфейс ---
+root = tk.Tk()
+root.title("Chat DDT")
+
+text = tk.Text(root, height=40, width=100)
+text.pack()
+
+frame = tk.Frame(root)
+frame.pack()
+
+entry = tk.Entry(frame, width = 80)
+
+entry.pack(side="left")
+
+def send():
+    msg = entry.get() + "\n"
+    if msg:
+        sock.sendall(msg.encode())
+        entry.delete(0, tk.END)
+
+
+btn = tk.Button(frame, text="Отправить", command=send)
+btn.pack(side="left")
+
+def recive():
+    while True:
+        try:
+            data = sock.recv(1024)
+            if not data:
+                break
+
+            message = data.decode()
+
+            text.insert(tk.END, message)
+
+            text.see(tk.END)
+        except:
+            break
+
+threading.Thread(target=recive, daemon=True).start()
+
+root.mainloop()
